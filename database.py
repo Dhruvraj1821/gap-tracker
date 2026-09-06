e410921a8b01340b2fdeae7f36025d155fd4225a
Dhruvraj1821 <dhruvraj18211821@gmail.com>
2026-09-06 17:23:30 +0530

    fix: production and development db log fix

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
 DATABASE_URL = DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:devpassword@localhost/gaptracker")
 
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+IS_PRODUCTION = os.getenv("ENVIRONMENT", "development") == "production"
+engine = create_async_engine(DATABASE_URL, echo=not IS_PRODUCTION)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
